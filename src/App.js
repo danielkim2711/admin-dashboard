3af5aeb8a49d65144176a0f567ce72ca521dfa46
Daniel Kim <danielkim2711@gmail.com>
2022-03-10 17:27:06 +1300

    Add dashboard menu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import { Admin, Resource } from 'react-admin';
-import restProvider from 'ra-data-simple-rest';
+import simpleRestProvider from 'ra-data-simple-rest';
 import DogList from './components/dogs/DogList';
 import DogRegister from './components/dogs/DogRegister';
 import DogEdit from './components/dogs/DogEdit';
 import UserList from './components/users/UserList';
 import UserRegister from './components/users/UserRegister';
 import UserEdit from './components/users/UserEdit';
-import { Layout } from './Layout';
+import PetsIcon from '@mui/icons-material/Pets';
+import PersonIcon from '@mui/icons-material/Person';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Admin layout={Layout} dataProvider={restProvider('http://localhost:3000')}>
+    <Admin
+      dashboard={Dashboard}
+      dataProvider={simpleRestProvider('http://localhost:3000')}
+    >
       <Resource
         name='dogs'
         list={DogList}
         create={DogRegister}
         edit={DogEdit}
+        icon={PetsIcon}
       />
       <Resource
         name='users'
         list={UserList}
         create={UserRegister}
         edit={UserEdit}
+        icon={PersonIcon}
       />
     </Admin>
   );
--- src/Layout.js
+++
@@ -1,4 +0,0 @@
-import { Layout as LayoutItem } from 'react-admin';
-import { Menu } from './Menu';
-
-export const Layout = (props) => <LayoutItem {...props} menu={Menu} />;
